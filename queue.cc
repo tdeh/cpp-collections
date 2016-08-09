8a4c8735b6e3420bff61710bfd9543fc24afd991
Taylor DeHaan <tdehaan93@gmail.com>
2016-08-08 19:26:47 -0600

    Implemented enqueue()

--- queue.cc
+++ queue.cc
@@ -1,1 +1,14 @@
 #include "queue.h"
+
+Queue::Queue(size_t size) : capacity_(capacity), arr[capacity] {
+    front_ = rear_ = size_ = 0;
+}
+
+void Queue::enqueue(int item) {
+    if (isFull()) {
+        throw -1;
+    }
+
+    ++rear_;
+    arr_[rear_] = item;
+}
--- queue.h
+++ queue.h
@@ -3,13 +3,20 @@
 
 class Queue {
     public:
-        explicit Queue(size_t size);
-        ~Queue();
+        explicit Queue(size_t capacity);
+        virtual ~Queue();
         void enqueue(int item);
         void dequeue(int item);
         size_t size();
         bool isEmpty();
         int front();
+
+    private:
+        size_t capacity_;
+        size_t size_;
+        int arr_[];
+        unsigned int front_;
+        unsigned int rear_;
 };
 
 #endif // QUEUE_H

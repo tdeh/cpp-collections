06f8f718d0d3cacf0b27bf26330ca6a16a3381d9
Taylor DeHaan <tdehaan93@gmail.com>
2016-08-25 19:49:54 -0600

    Changed Adjacency List to extend graph impl

--- graph/adjacency_list.h
+++ graph/adjacency_list.h
@@ -1,16 +1,21 @@
 #ifndef ADJACENCY_LIST_H
 #define ADJACENCY_LIST_H
 
-class AdjancencyList {
+#include <map>
+#include <vector>
+#include "vertex.h"
+#include "graph_impl.h"
+
+class AdjancencyList : public GraphImpl {
     public:
         AdjancencyList();
         virtual ~AdjancencyList() {};
-        void AddEdge(Vertex* v);
-        void AddNeighbor(Vertex* v, Vertex* w);
-        std::vector<Vertex*>* GetNeighbors(Vertex* v);
 
     private:
-        std::map<Vertex*, std::vector<Vertex*>> adjancency_map_;
+        void RemoveNeighbor(const Vertex* v, const std::vector<Vertex*>* neighbors);
+        std::vector<Vertex*>::iterator FindVertex(const Vertex* v, const std::vector<Vertex*>* neighbors);
+
+        std::map<Vertex*, std::vector<Vertex*>> vertex_map_;
 }
 
 #endif // ADJACENCY_LIST_H
